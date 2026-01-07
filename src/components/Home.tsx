029525f4c49b9ef585a048396290c7a6e24f9ec3
KaikyVictorio <kaikyvictorio2006@gmail.com>
2026-01-07 19:28:07 -0300

    new imgs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,137 @@
+import { useEffect, useRef, useState } from "react";
+import "../global.css"
 
-function Home () {
-  return (
- <>
- <p>home</p>
- </>
-  );
+type NavItem = {
+  icon: string;
+  alt: string;
 };
 
-export default Home;
+export default function App() {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const itemsRef = useRef<(HTMLButtonElement | null)[]>([]);
+  const indicatorRef = useRef<HTMLSpanElement | null>(null);
+
+  const navItems: NavItem[] = [
+    { icon: "assets/notifications.png", alt: "Adicionar" },
+    { icon: "assets/Home.png", alt: "Home" },
+    { icon: "assets/User.png", alt: "Perfil" },
+  ];
+
+  useEffect(() => {
+    const activeItem = itemsRef.current[activeIndex];
+
+    if (activeItem && indicatorRef.current) {
+      indicatorRef.current.style.left = `${activeItem.offsetLeft}px`;
+    }
+  }, [activeIndex]);
+
+  return (
+    <>
+      <header className="header">
+        <div className="header-content">
+          <img src="assets/logo.svg" alt="Logo" className="logo" />
+          <div className="titulo1">
+            <span>Olá,</span>
+            <strong>pecuarista!</strong>
+          </div>
+        </div>
+      </header>
+
+      <main className="main">
+        <section className="filtros">
+          <button className="filtro ativo">Críticos</button>
+          <button className="filtro">Mais recentes</button>
+          <button className="filtro">Mais antigos</button>
+          <button className="filtro">A – Z</button>
+        </section>
+
+        <section className="area-tanques">
+          <h2 className="titulo">Selecione seus tanques</h2>
+
+          <section className="container-tanque">
+            <h3 className="nome-tanque">Tanque Barriguinha Mole</h3>
+            <div className="conteudo-tanque">
+              <div className="box-tanque">
+                <img src="assets/tanque.jpg" alt="Tanque" className="icone-tanque" />
+              </div>
+              <img src="assets/gauge.jpg" alt="Indicador" className="icone-gauge" />
+            </div>
+            <div className="legendas">
+              <div className="info"><span>Volume</span><strong>10.000 L</strong></div>
+              <div className="info"><span>pH</span><strong>6,7</strong></div>
+              <div className="info"><span>Temperatura</span><strong>4°C</strong></div>
+            </div>
+          </section>
+
+          <section className="container-tanque">
+            <h3 className="nome-tanque">Tanque Barriguinha Dura</h3>
+            <div className="conteudo-tanque">
+              <div className="box-tanque">
+                <img src="assets/tanque2.png" alt="Tanque" className="icone-tanque" />
+              </div>
+              <img src="assets/gauge.jpg" alt="Indicador" className="icone-gauge" />
+            </div>
+            <div className="legendas">
+              <div className="info"><span>Volume</span><strong>15.000 L</strong></div>
+              <div className="info"><span>pH</span><strong>6,3</strong></div>
+              <div className="info"><span>Temperatura</span><strong>3.8°C</strong></div>
+            </div>
+          </section>
+
+          <section className="container-tanque">
+            <h3 className="nome-tanque">Tanque Piracanjuba</h3>
+            <div className="conteudo-tanque">
+              <div className="box-tanque">
+                <img src="assets/tanque3.png" alt="Tanque" className="icone-tanque" />
+              </div>
+              <img src="assets/gauge.jpg" alt="Indicador" className="icone-gauge" />
+            </div>
+            <div className="legendas">
+              <div className="info"><span>Volume</span><strong>12.000 L</strong></div>
+              <div className="info"><span>pH</span><strong>6,5</strong></div>
+              <div className="info"><span>Temperatura</span><strong>3.9°C</strong></div>
+            </div>
+          </section>
+
+          <section className="container-tanque">
+            <h3 className="nome-tanque">Tanque Mimosa</h3>
+            <div className="conteudo-tanque">
+              <div className="box-tanque">
+                <img src="" alt="Tanque" className="icone-tanque" />
+              </div>
+              <img src="assets/gauge.jpg" alt="Indicador" className="icone-gauge" />
+            </div>
+            <div className="legendas">
+              <div className="info"><span>Volume</span><strong>16.000 L</strong></div>
+              <div className="info"><span>pH</span><strong>6,7</strong></div>
+              <div className="info"><span>Temperatura</span><strong>4°C</strong></div>
+            </div>
+          </section>
+        </section>
+
+        <section className="navbar-wrapper">
+          <nav className="navbar">
+            {navItems.map((item, index) => (
+              <button
+                key={index}
+                ref={(el: HTMLButtonElement | null) => {
+                  itemsRef.current[index] = el;
+                }}
+                className={`nav-item ${activeIndex === index ? "active" : ""}`}
+                onClick={() => setActiveIndex(index)}
+              >
+                <img src={item.icon} alt={item.alt} />
+              </button>
+            ))}
+            <span ref={indicatorRef} className="nav-indicator"></span>
+          </nav>
+        </section>
+      </main>
+
+      <footer>
+        <p className="footer"></p>
+      </footer>
+    </>
+  );
+}
