896116570b79ed9ef0fcb784a1b969a2e1f81a22
KaikyVictorio <kaikyvictorio2006@gmail.com>
2026-01-16 22:10:50 -0300

    new screen and add responsive

---
+++ splash.tsx
@@ -0,0 +1,14 @@
+import "../global.css";
+import SplashScreen from "../components/SplashScreen/splashscreen";
+
+function telaSplash() {
+  return (
+    <>
+      <div className="splash-container">
+        <SplashScreen />
+      </div>
+    </>
+  );
+}
+
+export default telaSplash;
